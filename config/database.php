996bb5cf3990310cc6b78d20ee7c514c9aa932d6
muchoco <aya@lachelier.com>
2020-03-30 14:25:32 +0900

    CircleCI用のDB設定変更

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'circleci_test',
+            'database' => 'circle_test',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
