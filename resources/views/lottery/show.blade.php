b9ec8699ccbe0ceec14cb2d69d7f18b16ed7a1cf
muchoco <aya@lachelier.com>
2020-04-05 18:39:41 +0900

    くじ引きページのview

--- resources/views/lottery/show.blade.php
+++ resources/views/lottery/show.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'くじをひこう')
 @section('content')
 <div class="container">
-    <div>
-        <img src="https://placehold.jp/150x150.png" class="rounded mx-auto d-block" alt="説明っぽい画像">
-        <a class="btn btn-primary" href="/k/{{ $lottery->uname }}/result">くじを引く</a>
+    <img src="https://placehold.jp/250x250.png" class="rounded mx-auto d-block" alt="説明っぽい画像">
+    <div class="text-center">
+        <a class="btn btn-primary mt-3" href="/k/{{ $lottery->uname }}/result">くじを引く</a>
     </div>
 </div>
 @endsection
